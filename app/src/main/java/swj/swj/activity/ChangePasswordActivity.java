981f014a1a42cbbf0cf8aac04f2de204ff6353d6
cntoplolicon <cntoplolicon@gmail.com>
2015-09-17 16:19:36 +0800

    change password

--- app/src/main/java/swj/swj/activity/ChangePasswordActivity.java
+++ app/src/main/java/swj/swj/activity/ChangePasswordActivity.java
@@ -2,22 +2,29 @@ package swj.swj.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Response;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import swj.swj.R;
+import swj.swj.common.CommonMethods;
+import swj.swj.common.JsonErrorListener;
 import swj.swj.common.LocalUserInfo;
+import swj.swj.common.RestClient;
 
 /**
  * Created by jiewei on 9/4/15.
  */
 public class ChangePasswordActivity extends Activity {
 
-    private EditText etOldPwd, etNewPwd, etNewPwdRe;
-    private Button btnSavePwdChange;
+    private EditText etOldPwd, etNewPwd, etNewPwdConfirm;
     private String testPassword = LocalUserInfo.getInstance().getUserInfo("password");
 
     public void onCreate(Bundle savedInstanceState) {
@@ -27,22 +34,62 @@ public class ChangePasswordActivity extends Activity {
     }
 
     private void initView() {
-        etOldPwd = (EditText) findViewById(R.id.et_old_pwd);
-        etNewPwd = (EditText) findViewById(R.id.et_new_pwd);
-        etNewPwdRe = (EditText) findViewById(R.id.et_new_pwd_repeat);
-        btnSavePwdChange = (Button) findViewById(R.id.btn_change_pwd_save);
-        btnSavePwdChange.setOnClickListener(new View.OnClickListener() {
+        etOldPwd = (EditText) findViewById(R.id.et_old_password);
+        etNewPwd = (EditText) findViewById(R.id.et_password);
+        etNewPwdConfirm = (EditText) findViewById(R.id.et_password_confirmation);
+    }
+
+    public void onSubmit(View view) {
+        if (!inputValidation()) {
+            return;
+        }
+
+        String oldPassword = etOldPwd.getText().toString();
+        String password = etNewPwd.getText().toString();
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("old_password", oldPassword);
+        attributes.put("password", password);
+
+        RestClient.getInstance().updateUserAttributes(attributes, new Response.Listener<JSONObject>() {
             @Override
-            public void onClick(View v) {
-                UpdatePassword();
+            public void onResponse(JSONObject response) {
+                finish();
             }
-        });
+        }, new JsonErrorListener(getApplicationContext(), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject errors) {
+                CommonMethods.toastError(getApplicationContext(), errors, "old_password");
+                CommonMethods.toastError(getApplicationContext(), errors, "password");
+            }
+        }));
+    }
+
+    private boolean inputValidation() {
+        String oldPassword = etOldPwd.getText().toString();
+        if (!CommonMethods.isValidPwd(oldPassword)) {
+            Toast.makeText(this, R.string.password_invalid_format, Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        String password = etNewPwd.getText().toString();
+        if (!CommonMethods.isValidPwd(password)) {
+            Toast.makeText(this, R.string.password_invalid_format, Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        String passwordConfirmation = etNewPwdConfirm.getText().toString();
+        if (!passwordConfirmation.equals(password)) {
+            Toast.makeText(this, R.string.password_confirmation_incorrect, Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
     }
 
     private void UpdatePassword() {
         String OldPwd = etOldPwd.getText().toString().trim();
         String NewPwd = etNewPwd.getText().toString().trim();
-        String NewPwdRe = etNewPwdRe.getText().toString().trim();
+        String NewPwdRe = etNewPwdConfirm.getText().toString().trim();
         if (!OldPwd.equals(testPassword)) {
             Toast.makeText(getApplicationContext(), getResources().getString(R.string.password_incorrect), Toast.LENGTH_SHORT).show();
         } else if (!NewPwd.equals(NewPwdRe)) {
