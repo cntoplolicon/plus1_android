6185f30fe9bec8b8cc3253583d088b56496c5cd4
cntoplolicon <cntoplolicon@gmail.com>
2015-09-18 10:14:03 +0800

    modify user biography

--- app/src/main/java/swj/swj/activity/UpdateSignActivity.java
+++ app/src/main/java/swj/swj/activity/UpdateSignActivity.java
@@ -3,45 +3,54 @@ package swj.swj.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import com.android.volley.Response;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import swj.swj.R;
-import swj.swj.common.LocalUserInfo;
+import swj.swj.common.JsonErrorListener;
+import swj.swj.common.RestClient;
+import swj.swj.model.User;
 
 /**
  * Created by jiewei on 9/4/15.
  */
 public class UpdateSignActivity extends Activity {
 
-    private TextView tvSaveSign;
-    private EditText etSign;
-    private String newSign;
+    private EditText biographyInput;
+
+    private View.OnClickListener onSubmit = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String biography = biographyInput.getText().toString();
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("biography", biography);
+            RestClient.getInstance().updateUserAttributes(attributes,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            User.updateCurrentUser(response.toString());
+                            finish();
+                        }
+                    }, new JsonErrorListener(getApplicationContext(), null));
+        }
+    };
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_sign);
 
-        final String sign = LocalUserInfo.getInstance().getUserInfo("sign");
-        etSign = (EditText) findViewById(R.id.et_sign);
-        etSign.setText(sign);
-        tvSaveSign = (TextView) findViewById(R.id.tv_sign_save);
-        tvSaveSign.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                newSign = etSign.getText().toString().trim();
-                if (sign.equals(newSign) || newSign.isEmpty()) {
-                    return;
-                }
-                temp_updateSign(newSign);
-            }
-        });
+        biographyInput = (EditText) findViewById(R.id.et_biography);
+        biographyInput.setText(User.current.getBiography());
 
-    }
-
-    private void temp_updateSign(String newSign) {
-        LocalUserInfo.getInstance().setUserInfo("sign", newSign);
-        finish();
+        Button submitButton = (Button) findViewById(R.id.btn_submit);
+        submitButton.setOnClickListener(onSubmit);
     }
 
     public void back(View view) {
