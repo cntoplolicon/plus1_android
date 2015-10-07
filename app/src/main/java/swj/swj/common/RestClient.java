be314557bc65ac55f9769f46614919fe80411bd4
cntoplolicon <cntoplolicon@gmail.com>
2015-10-07 16:51:31 +0800

    revert server ip

--- app/src/main/java/swj/swj/common/RestClient.java
+++ app/src/main/java/swj/swj/common/RestClient.java
@@ -31,7 +31,7 @@ import swj.swj.model.User;
  */
 public class RestClient {
 
-    private static final String DEBUG_SERVER_URL = "http://192.168.1.147:9393";
+    private static final String DEBUG_SERVER_URL = "http://192.168.1.122:9393";
     private static final String RELEASE_SERVER_URL = "http://liuxingapp:3000";
     private static final boolean POST_VIEWS_ENABLED = true;
 
