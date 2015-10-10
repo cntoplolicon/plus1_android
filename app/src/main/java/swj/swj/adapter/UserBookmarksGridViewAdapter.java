8e81f7efb57437f12316146e97e2446dc539f2a5
jiewei1988 <499155526@qq.com>
2015-10-10 15:44:57 +0800

    update bookmark cache in User bookmard gridview adapter

--- app/src/main/java/swj/swj/adapter/UserBookmarksGridViewAdapter.java
+++ app/src/main/java/swj/swj/adapter/UserBookmarksGridViewAdapter.java
@@ -2,11 +2,13 @@ package swj.swj.adapter;
 
 import android.content.Context;
 
-import com.android.volley.Response;
-
 import org.jdeferred.DoneCallback;
 import org.json.JSONArray;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import swj.swj.common.BookmarkService;
 import swj.swj.common.CommonMethods;
 import swj.swj.common.JsonErrorListener;
 import swj.swj.common.RestClient;
@@ -24,6 +26,11 @@ public class UserBookmarksGridViewAdapter extends PostsGridViewAdapter {
                     public void onDone(JSONArray response) {
                         posts = CommonMethods.createDefaultGson().fromJson(response.toString(), Post[].class);
                         notifyDataSetChanged();
+                        List<Integer> postIds = new ArrayList<Integer>(posts.length);
+                        for (Post p : posts) {
+                            postIds.add(p.getId());
+                        }
+                        BookmarkService.getInstance().updateBookmarks(postIds);
                     }
                 }).fail(new JsonErrorListener(context, null));
     }
--- app/src/main/java/swj/swj/common/BookmarkService.java
+++ app/src/main/java/swj/swj/common/BookmarkService.java
@@ -37,5 +37,4 @@ public class BookmarkService {
         bookmarkedPostsIds.addAll(postIds);
     }
 
-
 }
