8c800210e746d093655fe6601d72f2f76f77b154
caodooGMT <496555782@qq.com>
2015-10-12 19:35:20 +0800

    bookmark cache update

--- app/src/main/java/swj/swj/adapter/UserBookmarksGridViewAdapter.java
+++ app/src/main/java/swj/swj/adapter/UserBookmarksGridViewAdapter.java
@@ -5,9 +5,6 @@ import android.content.Context;
 import org.jdeferred.DoneCallback;
 import org.json.JSONArray;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import swj.swj.common.BookmarkService;
 import swj.swj.common.CommonMethods;
 import swj.swj.common.JsonErrorListener;
@@ -18,19 +15,17 @@ import swj.swj.model.Post;
  * Created by cntoplolicon on 10/7/15.
  */
 public class UserBookmarksGridViewAdapter extends PostsGridViewAdapter {
+
     public UserBookmarksGridViewAdapter(Context context) {
         super(context);
+        posts = BookmarkService.getInstance().getBookmarkedPosts();
         RestClient.getInstance().getUserBookmarks().done(
                 new DoneCallback<JSONArray>() {
                     @Override
                     public void onDone(JSONArray response) {
                         posts = CommonMethods.createDefaultGson().fromJson(response.toString(), Post[].class);
                         notifyDataSetChanged();
-                        List<Integer> postIds = new ArrayList<Integer>(posts.length);
-                        for (Post p : posts) {
-                            postIds.add(p.getId());
-                        }
-                        BookmarkService.getInstance().updateBookmarks(postIds);
+                        BookmarkService.getInstance().updateBookmarkCache(posts);
                     }
                 }).fail(new JsonErrorListener(context, null));
     }
