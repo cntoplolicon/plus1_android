0c8e36fc7089d61ac012b8a591ca6dbfd9a1f1a1
caodooGMT <496555782@qq.com>
2015-10-13 21:22:50 +0800

    post comment

--- app/src/main/java/swj/swj/activity/CardDetailsActivity.java
+++ app/src/main/java/swj/swj/activity/CardDetailsActivity.java
@@ -2,8 +2,10 @@ package swj.swj.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,11 +18,7 @@ import org.jdeferred.DoneCallback;
 import org.jdeferred.Promise;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
-import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -28,7 +26,6 @@ import butterknife.OnClick;
 import swj.swj.R;
 import swj.swj.adapter.CardDetailsAdapter;
 import swj.swj.application.SnsApplication;
-import swj.swj.bean.CardDetailsItemBean;
 import swj.swj.common.BookmarkService;
 import swj.swj.common.CommonMethods;
 import swj.swj.common.JsonErrorListener;
@@ -38,7 +35,6 @@ import swj.swj.model.Comment;
 import swj.swj.model.Post;
 
 public class CardDetailsActivity extends Activity {
-    private List<CardDetailsItemBean> mList;
 
     @Bind(R.id.iv_image)
     ImageView ivImage;
@@ -52,9 +48,11 @@ public class CardDetailsActivity extends Activity {
     TextView tvTime;
     @Bind(R.id.iv_bookmark)
     ImageView ivBookmark;
+    @Bind(R.id.et_new_comment)
+    EditText etNewComment;
 
     private Post post;
-    private Comment[] comments;
+    private CardDetailsAdapter cardDetailsAdapter;
 
 
     @Override
@@ -92,7 +90,6 @@ public class CardDetailsActivity extends Activity {
     }
 
     private void initData() {
-        mList = new ArrayList<>();
         String postJson = getIntent().getStringExtra("post_json");
         post = CommonMethods.createDefaultGson().fromJson(postJson, Post.class);
 
@@ -100,7 +97,7 @@ public class CardDetailsActivity extends Activity {
         lvListView.setDividerHeight(0);
         View headerView = LayoutInflater.from(this).inflate(R.layout.card_details_header, null);
         lvListView.addHeaderView(headerView, null, false);
-        CardDetailsAdapter cardDetailsAdapter = new CardDetailsAdapter(this, post, mList);
+        cardDetailsAdapter = new CardDetailsAdapter(this, post);
         lvListView.setAdapter(cardDetailsAdapter);
     }
 
@@ -139,6 +136,35 @@ public class CardDetailsActivity extends Activity {
                     });
         }
 
+    }
+
+    @OnClick(R.id.btn_send_comment)
+    public void onSendCommentClicked(View view) {
+        if (etNewComment.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.comment_text_required), Toast.LENGTH_LONG).show();
+            return;
+        }
+        view.setEnabled(false);
+        RestClient.getInstance().newComment(etNewComment.getText().toString(), -1, post.getId())
+                .done(new DoneCallback<JSONObject>() {
+                    @Override
+                    public void onDone(JSONObject result) {
+                        etNewComment.setText("");
+                        Toast.makeText(getApplicationContext(), R.string.comment_success, Toast.LENGTH_LONG).show();
+                        Comment newComment = CommonMethods.createDefaultGson().fromJson(result.toString(), Comment.class);
+                        cardDetailsAdapter.add(newComment);
+                        cardDetailsAdapter.notifyDataSetChanged();
+                    }
+                })
+                .fail(new JsonErrorListener(getApplicationContext(), null) {
+                    @Override
+                    public void onFail(VolleyError error) {
+                        super.onFail(error);
+                        Log.e(PublishActivity.class.getName(), "failed uploading posts", error);
+                        Toast.makeText(getApplicationContext(), R.string.comment_failure, Toast.LENGTH_LONG).show();
+                    }
+                })
+                .always(new ResetViewClickable<JSONObject, VolleyError>(view));
     }
 
     private void syncBookmarkInfo() {
--- app/src/main/java/swj/swj/adapter/CardDetailsAdapter.java
+++ app/src/main/java/swj/swj/adapter/CardDetailsAdapter.java
@@ -4,17 +4,14 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.jdeferred.DoneCallback;
 import org.json.JSONArray;
 
-import java.util.List;
-
 import swj.swj.R;
-import swj.swj.bean.CardDetailsItemBean;
 import swj.swj.common.CommonMethods;
 import swj.swj.common.JsonErrorListener;
 import swj.swj.common.RestClient;
@@ -24,34 +21,20 @@ import swj.swj.model.Post;
 /**
  * Created by shw on 2015/9/14.
  */
-public class CardDetailsAdapter extends BaseAdapter {
-    private List<CardDetailsItemBean> mList;
+public class CardDetailsAdapter extends ArrayAdapter<Comment> {
     private LayoutInflater mInflater;
-    private Comment[] comments = new Comment[]{};
 
-    public CardDetailsAdapter(Context context, Post post, List<CardDetailsItemBean> list) {
-        mList = list;
+    public CardDetailsAdapter(Context context, Post post) {
+        super(context, 0);
         RestClient.getInstance().getPostComments(post.getId()).done(new DoneCallback<JSONArray>() {
             @Override
             public void onDone(JSONArray response) {
-                comments = CommonMethods.createDefaultGson().fromJson(response.toString(), Comment[].class);
+                Comment[] comments = CommonMethods.createDefaultGson().fromJson(response.toString(), Comment[].class);
+                CardDetailsAdapter.this.addAll(comments);
                 notifyDataSetChanged();
-                for (Comment comment : comments) {
-                    mList.add(new CardDetailsItemBean(R.drawable.default_useravatar, comment.getUser().getNickname(), comment.getContent()));
-                }
             }
         }).fail(new JsonErrorListener(context, null));
         mInflater = LayoutInflater.from(context);
-    }
-
-    @Override
-    public int getCount() {
-        return mList.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return mList.get(position);
     }
 
     @Override
@@ -73,10 +56,10 @@ public class CardDetailsAdapter extends BaseAdapter {
             viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        CardDetailsItemBean bean = mList.get(position);
-        viewHolder.imageView.setImageResource(bean.getImageView());
-        viewHolder.userName.setText(bean.getUserName());
-        viewHolder.context.setText(bean.getContent());
+        Comment comment = getItem(position);
+        viewHolder.imageView.setImageResource(R.drawable.default_useravatar);
+        viewHolder.userName.setText(comment.getUser().getNickname());
+        viewHolder.context.setText(comment.getContent());
         return convertView;
     }
 
