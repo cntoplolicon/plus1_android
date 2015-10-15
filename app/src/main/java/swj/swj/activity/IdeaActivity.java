a2575a0d0bbbfa564c15ad0e72ee2196c7581500
haiwei27 <haiwei@caodoo.com>
2015-10-15 14:22:45 +0800

    changes ideaActivity

--- app/src/main/java/swj/swj/activity/IdeaActivity.java
+++ app/src/main/java/swj/swj/activity/IdeaActivity.java
@@ -21,32 +21,8 @@ import swj.swj.model.User;
 
 public class IdeaActivity extends Activity {
 
-    @Bind(R.id.et_biography)
-    EditText biographyInput;
-    @Bind(R.id.et_qq_phone)
-    EditText qqPhoneInput;
-
-    @OnClick(R.id.btn_submit)
-    public void submit() {
-        String biography = biographyInput.getText().toString();
-        String qqPhone = qqPhoneInput.getText().toString();
-        Map<String, Object> attributes = new HashMap<>();
-        attributes.put("biography", biography);
-        attributes.put("qqPhone", qqPhone);
-        RestClient.getInstance().updateUserAttributes(attributes).done(
-                new DoneCallback<JSONObject>() {
-                    @Override
-                    public void onDone(JSONObject response) {
-                        User.updateCurrentUser(response.toString());
-                        Toast.makeText(IdeaActivity.this, "意见反馈成功", Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-                }).fail(new JsonErrorListener(getApplicationContext(), null));
-    }
-
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_idea);
-        ButterKnife.bind(this);
     }
 }
