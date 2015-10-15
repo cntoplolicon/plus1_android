4696f2bbee38b0ede45177394d2a3a141fc9f17a
caodooGMT <496555782@qq.com>
2015-10-15 19:40:51 +0800

    comment order dfs

--- app/src/main/java/swj/swj/adapter/CardDetailsAdapter.java
+++ app/src/main/java/swj/swj/adapter/CardDetailsAdapter.java
@@ -11,8 +11,12 @@ import android.widget.TextView;
 import org.jdeferred.DoneCallback;
 import org.json.JSONArray;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import swj.swj.R;
 import swj.swj.common.CommonMethods;
@@ -77,42 +81,47 @@ public class CardDetailsAdapter extends ArrayAdapter<Comment> {
         for (int i = 0; i < comments.length; i++) {
             comments[i] = getItem(i);
         }
-        Arrays.sort(comments, new Comparator<Comment>() {
-            @Override
-            public int compare(Comment comment1, Comment comment2) {
-                if (findCommentAncestor(comment1, comments).equals(findCommentAncestor(comment2, comments))) {
-                    return comment1.getId() - comment2.getId();
-                } else {
-                    return findCommentAncestor(comment1, comments).getId() - findCommentAncestor(comment2, comments).getId();
-                }
+        Arrays.sort(comments, new commentsPreprocessing());
+
+        Map<Integer, List<Comment>> nodeToChildrenMap = new HashMap<>();
+        for (Comment comment : comments) {
+            nodeToChildrenMap.put(comment.getId(), new ArrayList<Comment>());
+        }
+        for (Comment comment : comments) {
+            if (comment.getReplyToId() > 0) {
+                nodeToChildrenMap.get(comment.getReplyToId()).add(comment);
             }
-        });
+        }
         clear();
-        addAll(comments);
-        notifyDataSetChanged();
-    }
-
-    private Comment findCommentAncestor(Comment comment, Comment[] comments) {
-        if (comment.getReplyToId() != 0) {
-            return findCommentAncestor(findCommentParent(comment, comments), comments);
-        }
-        return comment;
-    }
-
-    private Comment findCommentParent(Comment comment, Comment[] comments) {
-        for (Comment tmpComment : comments) {
-            if (tmpComment.getId() == comment.getReplyToId()) {
-                comment = tmpComment;
+        for (Comment comment : comments) {
+            if (comment.getReplyToId() == 0) {
+                depthFirstSearch(nodeToChildrenMap, comment);
             }
         }
-        return comment;
     }
 
+    private void depthFirstSearch(Map<Integer, List<Comment>> nodeToChildrenMap, Comment comment) {
+        add(comment);
+        for (Comment reply : nodeToChildrenMap.get(comment.getId())) {
+            depthFirstSearch(nodeToChildrenMap, reply);
+        }
+    }
 
     private static class ViewHolder {
         private ImageView imageView;
         private TextView userName;
         private TextView context;
+    }
+
+    private class commentsPreprocessing implements Comparator<Comment> {
+
+        @Override
+        public int compare(Comment comment1, Comment comment2) {
+            if (comment1.getCreatedAt().equals(comment2.getCreatedAt())) {
+                return comment1.getId() - comment2.getId();
+            }
+            return comment1.getCreatedAt().compareTo(comment2.getCreatedAt());
+        }
     }
 
 }
