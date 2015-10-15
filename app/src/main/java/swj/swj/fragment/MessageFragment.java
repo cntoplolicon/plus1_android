d3de3ce4f6197b7b35fe23fedd6f875fe8a69c8a
jiewei1988 <499155526@qq.com>
2015-10-15 19:50:13 +0800

    use getMyNotifications and order by sql

--- app/src/main/java/swj/swj/fragment/MessageFragment.java
+++ app/src/main/java/swj/swj/fragment/MessageFragment.java
@@ -9,13 +9,13 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import java.util.Collections;
 import java.util.List;
 
 import swj.swj.R;
 import swj.swj.activity.CardDetailsActivity;
 import swj.swj.adapter.MessageAdapter;
 import swj.swj.model.Notification;
+import swj.swj.model.User;
 
 
 public class MessageFragment extends Fragment {
@@ -25,8 +25,7 @@ public class MessageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_message, container, false);
-        notifications = Notification.getAll();
-        Collections.reverse(notifications);
+        notifications = Notification.getMyNotifications(User.current.getId());
         final MessageAdapter messageAdapter = new MessageAdapter(this.getActivity(), notifications);
         ListView lvListView = (ListView) view.findViewById(R.id.lv_listView);
         lvListView.setAdapter(messageAdapter);
