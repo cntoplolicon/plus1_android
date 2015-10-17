643683949915f9ccf1afaf13758d6ad7a612c075
jiewei1988 <499155526@qq.com>
2015-10-17 11:24:27 +0800

    Add callback for notification Reveived

--- app/src/main/java/swj/swj/fragment/MessageFragment.java
+++ app/src/main/java/swj/swj/fragment/MessageFragment.java
@@ -3,7 +3,6 @@ package swj.swj.fragment;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,28 +14,40 @@ import java.util.List;
 import swj.swj.R;
 import swj.swj.activity.CardDetailsActivity;
 import swj.swj.adapter.MessageAdapter;
+import swj.swj.common.PushNotificationService;
 import swj.swj.model.Notification;
 import swj.swj.model.User;
 
 
 public class MessageFragment extends Fragment {
 
+    private MessageAdapter messageAdapter;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_message, container, false);
         List<Notification> notifications = Notification.getMyNotifications(User.current.getId());
-        MessageAdapter messageAdapter = new MessageAdapter(getActivity(), notifications);
+        messageAdapter = new MessageAdapter(getActivity(), notifications);
+        PushNotificationService.getInstance().setCallback(new NotificationChangedCallback());
         ListView lvListView = (ListView) view.findViewById(R.id.lv_listView);
         lvListView.setAdapter(messageAdapter);
         lvListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getActivity(), CardDetailsActivity.class);
-                intent.putExtra("notification", (Notification)view.getTag());
+                intent.putExtra("notification", (Notification) view.getTag());
                 startActivity(intent);
             }
         });
         return view;
+    }
+
+    private class NotificationChangedCallback implements PushNotificationService.Callback {
+
+        @Override
+        public void onNotificationChanged(Notification notification) {
+            messageAdapter.updateContent(notification);
+        }
     }
 
 }
