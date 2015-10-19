82db7fec8e7e637aa9597819492368c90b84e5f2
caodooGMT <496555782@qq.com>
2015-10-19 10:32:56 +0800

    little bug fix

--- app/src/main/java/swj/swj/adapter/CardDetailsAdapter.java
+++ app/src/main/java/swj/swj/adapter/CardDetailsAdapter.java
@@ -74,8 +74,10 @@ public class CardDetailsAdapter extends ArrayAdapter<Comment> {
 
         if (comment.getReplyToId() > 0) {
             float scale = getContext().getResources().getDisplayMetrics().density;
-            int PaddingDpAsPx = (int) (20 *scale + 0.5f);
+            int PaddingDpAsPx = (int) (20 * scale + 0.5f);
             view.setPadding(PaddingDpAsPx, 0, 0, 0);
+        } else {
+            view.setPadding(0, 0, 0, 0);
         }
         String avatarPath = comment.getUser().getAvatar();
         if (avatarPath == null || avatarPath.isEmpty()) {
