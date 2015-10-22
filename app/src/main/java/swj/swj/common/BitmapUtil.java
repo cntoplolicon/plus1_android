fa724da5d220b6babda7ec6ed963bdd2cca31838
cntoplolicon <cntoplolicon@gmail.com>
2015-10-22 10:04:42 +0800

    remove dead code

--- app/src/main/java/swj/swj/common/BitmapUtil.java
+++ app/src/main/java/swj/swj/common/BitmapUtil.java
@@ -11,7 +11,6 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.nostra13.universalimageloader.core.assist.ImageSize;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -25,7 +24,6 @@ public final class BitmapUtil {
 
     private static int MAX_IMAGE_WIDTH = 960;
     private static int MAX_IMAGE_HEIGHT = 960;
-    private static int MAX_IMAGE_BYTES = 150 * 1024;
 
     public static ImageFileInfo prepareBitmapForUploading(Context context, Uri uri) {
         File outputFile = getOutputFile();
@@ -88,37 +86,6 @@ public final class BitmapUtil {
                 .considerExifParams(true).imageScaleType(ImageScaleType.EXACTLY).build();
         return ImageLoader.getInstance().loadImageSync(uri.toString(),
                 imageSize, displayOptions);
-    }
-
-    private static ByteArrayOutputStream reduceQualityToMatchCapacity(Bitmap image, int capacity) {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        for (int quality = 100; quality > 0; quality -= 5) {
-            byteArrayOutputStream.reset();
-            image.compress(Bitmap.CompressFormat.JPEG, quality, byteArrayOutputStream);
-            if (byteArrayOutputStream.size() <= capacity) {
-                break;
-            }
-        }
-        return byteArrayOutputStream;
-    }
-
-    private static File saveCompressedBitmap(ByteArrayOutputStream byteArrayOutputStream, File file) {
-        FileOutputStream fileOutputStream = null;
-        try {
-            fileOutputStream = new FileOutputStream(file);
-            byteArrayOutputStream.writeTo(fileOutputStream);
-        } catch (IOException e) {
-            Log.e(BitmapUtil.class.getName(), "failed saving bitmap", e);
-        } finally {
-            try {
-                if (fileOutputStream != null) {
-                    fileOutputStream.close();
-                }
-            } catch (IOException e) {
-                Log.e(BitmapUtil.class.getName(), "failed closing fileOutputStream", e);
-            }
-        }
-        return file;
     }
 
     public static class ImageFileInfo {
