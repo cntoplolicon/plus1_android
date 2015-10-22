a86a739f4743eab31210d9119fc18d3fdba37427
caodooGMT <496555782@qq.com>
2015-10-22 14:16:23 +0800

    replace DeferredObject to throwable

--- app/src/main/java/swj/swj/fragment/HomeFragment.java
+++ app/src/main/java/swj/swj/fragment/HomeFragment.java
@@ -2,7 +2,6 @@ package swj.swj.fragment;
 
 import android.app.Fragment;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +9,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 import butterknife.Bind;
