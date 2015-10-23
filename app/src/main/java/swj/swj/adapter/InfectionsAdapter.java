71ffc8d3df8eef3c8ecc0c0dcfd27077775bafec
caodooGMT <496555782@qq.com>
2015-10-23 10:14:56 +0800

    infection adaper user nickname color via gender

--- app/src/main/java/swj/swj/adapter/InfectionsAdapter.java
+++ app/src/main/java/swj/swj/adapter/InfectionsAdapter.java
@@ -103,6 +103,17 @@ public class InfectionsAdapter {
 
         Post post = infection.getPost();
         itemViews.tvUser.setText(post.getUser().getNickname());
+        switch (post.getUser().getGender()) {
+            case User.GENDER_MALE:
+                itemViews.tvUser.setTextColor(view.getResources().getColor(R.color.personal_common_male_username));
+                break;
+            case User.GENDER_FEMALE:
+                itemViews.tvUser.setTextColor(view.getResources().getColor(R.color.personal_common_female_username));
+                break;
+            default:
+                itemViews.tvUser.setTextColor(view.getResources().getColor(R.color.unknown_gender));
+                break;
+        }
         int genderIcon = post.getUser().getGender() == User.GENDER_FEMALE ? R.drawable.icon_woman : R.drawable.icon_man;
         itemViews.tvUser.setCompoundDrawablesWithIntrinsicBounds(0, 0, genderIcon, 0);
         itemViews.tvContent.setText(post.getPostPages()[0].getText());
