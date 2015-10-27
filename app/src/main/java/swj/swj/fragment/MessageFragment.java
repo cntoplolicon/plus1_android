0dfcd84386f01947136538ad3997a43dfae55632
haiwei27 <haiwei@caodoo.com>
2015-10-27 10:38:21 +0800

    add no image state

--- app/src/main/java/swj/swj/fragment/MessageFragment.java
+++ app/src/main/java/swj/swj/fragment/MessageFragment.java
@@ -3,14 +3,18 @@ package swj.swj.fragment;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import swj.swj.R;
 import swj.swj.activity.CardDetailsActivity;
 import swj.swj.adapter.MessageAdapter;
@@ -23,14 +27,24 @@ public class MessageFragment extends Fragment {
     private PushNotificationService.Callback callback = new NotificationChangedCallback();
     private MessageAdapter messageAdapter;
 
+    @Bind(R.id.tv_no_message)
+    TextView tvNoMessage;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_message, container, false);
+
+        ButterKnife.bind(this,view);
 
         List<Notification> notifications = Notification.getMyNotifications(User.current.getId());
         messageAdapter = new MessageAdapter(getActivity(), notifications);
         ListView lvListView = (ListView) view.findViewById(R.id.lv_listView);
         lvListView.setAdapter(messageAdapter);
+        if (lvListView.getCount() > 0){
+            tvNoMessage.setVisibility(View.GONE);
+        }else{
+            tvNoMessage.setVisibility(View.VISIBLE);
+        }
         lvListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -41,7 +55,6 @@ public class MessageFragment extends Fragment {
         });
 
         PushNotificationService.getInstance().registerCallback(callback);
-
         return view;
     }
 
