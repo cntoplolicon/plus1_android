e721158b85121f25ebab53ae04b3c7daa13904c4
cntoplolicon <cntoplolicon@gmail.com>
2015-10-31 14:49:38 +0800

    umeng update app

--- app/src/main/java/swj/swj/application/SnsApplication.java
+++ app/src/main/java/swj/swj/application/SnsApplication.java
@@ -1,45 +1,29 @@
 package swj.swj.application;
 
-import android.app.AlertDialog;
 import android.app.Application;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.util.Log;
-import android.view.Display;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.activeandroid.ActiveAndroid;
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.umeng.update.UmengUpdateAgent;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
-import org.jdeferred.DoneCallback;
-import org.json.JSONObject;
-
 import swj.swj.BuildConfig;
-import swj.swj.R;
 import swj.swj.adapter.InfectionsAdapter;
-import swj.swj.common.CommonMethods;
-import swj.swj.common.JsonErrorListener;
 import swj.swj.common.LocalUserInfo;
 import swj.swj.common.PushNotificationService;
 import swj.swj.common.RestClient;
-import swj.swj.model.AppInfo;
 import swj.swj.model.User;
 
 /**
  * Created by cntoplolicon on 9/15/15.
  */
 public class SnsApplication extends Application {
-
-    private static AppInfo appInfo;
 
     public static final DisplayImageOptions DEFAULT_DISPLAY_OPTION = new DisplayImageOptions.Builder()
             .cacheInMemory(true)
@@ -59,7 +43,7 @@ public class SnsApplication extends Application {
         InfectionsAdapter.initialize(getApplicationContext());
         ActiveAndroid.initialize(getApplicationContext());
         loadCurrentUser();
-        loadAppInfo();
+        UmengUpdateAgent.update(this);
     }
 
     private void initImageLoader(Context context) {
@@ -80,55 +64,5 @@ public class SnsApplication extends Application {
         if (!userJson.isEmpty()) {
             User.updateCurrentUser(userJson);
         }
-    }
-
-    private void loadAppInfo() {
-        appInfo = new AppInfo();
-        RestClient.getInstance().getAppInfo().done(
-                new DoneCallback<JSONObject>() {
-                    @Override
-                    public void onDone(JSONObject response) {
-                        appInfo = CommonMethods.createDefaultGson().fromJson(response.toString(), AppInfo.class);
-                        try {
-                            int versionCode = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
-
-                            if (versionCode < appInfo.getVersionCode()) {
-                                updateApp();
-                            }
-                        } catch (PackageManager.NameNotFoundException e) {
-                            throw new IllegalStateException("version code must be specified in manifest", e);
-                        }
-                    }
-                }
-        ).fail(new JsonErrorListener(getApplicationContext(), null));
-    }
-
-    private void updateApp() {
-        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        alertDialog.setCanceledOnTouchOutside(true);
-        alertDialog.show();
-        Window window = alertDialog.getWindow();
-        WindowManager.LayoutParams lp = window.getAttributes();
-        Display display = window.getWindowManager().getDefaultDisplay();
-        lp.width = (int) (display.getWidth() * 0.8);
-        window.setContentView(R.layout.custom_dialog_update_versions);
-        TextView tvTitle = (TextView) window.findViewById(R.id.tv_title);
-        tvTitle.setText(R.string.update_versions_title);
-        TextView tvUnConfirm = (TextView) window.findViewById(R.id.tv_un_confirm);
-        tvUnConfirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                alertDialog.cancel();
-            }
-        });
-        TextView tvConfirm = (TextView) window.findViewById(R.id.tv_confirm);
-        tvConfirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "你真好，亲爱的", Toast.LENGTH_SHORT).show();
-            }
-        });
-        TextView tvReminder = (TextView) window.findViewById(R.id.tv_reminder);
-        window.setAttributes(lp);
     }
 }
