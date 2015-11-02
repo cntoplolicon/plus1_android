4343df3d534ae96a4f63c36d43720537916cf10e
haiwei27 <haiwei@caodoo.com>
2015-11-02 14:52:24 +0800

    auto height update dialog

---
+++ app/src/main/java/swj/swj/view/AutoHeightScrollView.java
@@ -0,0 +1,58 @@
+package swj.swj.view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+import swj.swj.R;
+
+/**
+ * Created by shw on 2015/11/2.
+ */
+public class AutoHeightScrollView extends ScrollView {
+    private int maxHeight;
+    private final int defaultHeight = 100;
+
+    public AutoHeightScrollView(Context context) {
+        super(context);
+    }
+
+    public AutoHeightScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        if (!isInEditMode()) {
+            init(context, attrs);
+        }
+    }
+
+    public AutoHeightScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        if (!isInEditMode()) {
+            init(context, attrs);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public AutoHeightScrollView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        if (!isInEditMode()) {
+            init(context, attrs);
+        }
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.AutoHeightScrollView);
+            maxHeight = styledAttrs.getDimensionPixelSize(R.styleable.AutoHeightScrollView_maxHeight, defaultHeight);
+            styledAttrs.recycle();
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(maxHeight, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
