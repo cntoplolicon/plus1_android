1101785a808052e98002210f6f96700d6bc1ddaf
syb0327 <yubo@caodoo.com>
2015-11-07 17:07:58 +0800

    umeng oauth

--- app/src/main/java/com/oneplusapp/activity/LoginActivity.java
+++ app/src/main/java/com/oneplusapp/activity/LoginActivity.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -19,24 +21,39 @@ import com.oneplusapp.common.JsonErrorListener;
 import com.oneplusapp.common.ResetViewClickable;
 import com.oneplusapp.common.RestClient;
 import com.oneplusapp.model.User;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.controller.UMServiceFactory;
+import com.umeng.socialize.controller.UMSocialService;
+import com.umeng.socialize.controller.listener.SocializeListeners;
+import com.umeng.socialize.exception.SocializeException;
+import com.umeng.socialize.sso.UMQQSsoHandler;
 
 import org.jdeferred.DoneCallback;
 import org.json.JSONObject;
+
+import java.util.Map;
+import java.util.Set;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends BaseActivity {
 
     private EditText usernameInput;
     private EditText passwordInput;
+    private UMSocialService mController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        ButterKnife.bind(this);
+        mController = UMServiceFactory.getUMSocialService("com.umeng.login");
+        UMQQSsoHandler qqSsoHandler = new UMQQSsoHandler(this, "1104950070", "qBwAJcdsf58q3PNf");
+        qqSsoHandler.addToSocialSDK();
         Button loginSubmit = (Button) findViewById(R.id.btn_submit);
         usernameInput = (EditText) findViewById(R.id.et_username);
         passwordInput = (EditText) findViewById(R.id.et_password);
-
         loginSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,6 +91,20 @@ public class LoginActivity extends BaseActivity {
         showGuideOnFirstLogin();
     }
 
+
+    @OnClick({R.id.iv_qq, R.id.iv_sina, R.id.iv_weixin})
+    public void login(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.iv_qq:
+                login(SHARE_MEDIA.QQ);
+                break;
+            case R.id.iv_sina:
+                login(SHARE_MEDIA.SINA);
+                break;
+        }
+    }
+
     private boolean inputValidation() {
         if (!CommonMethods.isValidUsername(usernameInput.getText().toString().trim())) {
             CommonDialog.showDialog(this, R.string.username_invalid_format);
@@ -95,5 +126,50 @@ public class LoginActivity extends BaseActivity {
                     GuideActivity.class));
             finish();
         }
+    }
+
+    public void login(SHARE_MEDIA platform) {
+        mController.doOauthVerify(this, platform, new SocializeListeners.UMAuthListener() {
+            @Override
+            public void onStart(SHARE_MEDIA platform) {
+                Toast.makeText(LoginActivity.this, "授权开始", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(SocializeException e, SHARE_MEDIA platform) {
+                Toast.makeText(LoginActivity.this, "授权错误", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onComplete(Bundle value, SHARE_MEDIA platform) {
+                Toast.makeText(LoginActivity.this, "授权完成", Toast.LENGTH_SHORT).show();
+                //获取相关授权信息
+                mController.getPlatformInfo(LoginActivity.this, platform, new SocializeListeners.UMDataListener() {
+                    @Override
+                    public void onStart() {
+                        Toast.makeText(LoginActivity.this, "获取平台数据开始...", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onComplete(int status, Map<String, Object> info) {
+                        if (status == 200 && info != null) {
+                            StringBuilder sb = new StringBuilder();
+                            Set<String> keys = info.keySet();
+                            for (String key : keys) {
+                                sb.append(key + "=" + info.get(key).toString() + "\r\n");
+                            }
+                            Toast.makeText(LoginActivity.this, sb.toString(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.d("TestData", "发生错误：" + status);
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onCancel(SHARE_MEDIA platform) {
+                Toast.makeText(LoginActivity.this, "授权取消", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
