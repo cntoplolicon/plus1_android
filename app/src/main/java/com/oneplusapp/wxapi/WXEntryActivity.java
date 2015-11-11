43cb9f16ade2cca12259bfb72f193fc914bb09f0
syb0327 <yubo@caodoo.com>
2015-11-11 19:32:57 +0800

    login activiy updated for oauth

--- app/src/main/java/com/oneplusapp/wxapi/WXEntryActivity.java
+++ app/src/main/java/com/oneplusapp/wxapi/WXEntryActivity.java
@@ -1,8 +1,18 @@
+<<<<<<< HEAD
+=======
+
+>>>>>>> login activiy updated for oauth
 package com.oneplusapp.wxapi;
 
 import com.umeng.socialize.weixin.view.WXCallbackActivity;
 
+<<<<<<< HEAD
 
 public class WXEntryActivity extends WXCallbackActivity {
 }
 
+=======
+public class WXEntryActivity extends WXCallbackActivity {
+
+}
+>>>>>>> login activiy updated for oauth
