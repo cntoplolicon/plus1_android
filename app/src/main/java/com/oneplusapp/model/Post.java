3e7e9de55fc9531921ad35a12916953e45af1c59
cntoplolicon <cntoplolicon@gmail.com>
2015-11-20 15:11:03 +0800

    add deleted helper method to posts

--- app/src/main/java/com/oneplusapp/model/Post.java
+++ app/src/main/java/com/oneplusapp/model/Post.java
@@ -69,6 +69,10 @@ public class Post {
         this.deletedBy = deletedBy;
     }
 
+    public boolean isDeleted() {
+        return deletedBy != null;
+    }
+
     public User getUser() {
         return user;
     }
