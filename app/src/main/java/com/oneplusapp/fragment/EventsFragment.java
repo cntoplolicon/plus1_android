645f3fadfd89d87d412e6bd17ac53f6c2d65ea31
caodooGMT <496555782@qq.com>
2015-12-02 16:00:37 +0800

    add event fragment

---
+++ app/src/main/java/com/oneplusapp/fragment/EventsFragment.java
@@ -0,0 +1,18 @@
+package com.oneplusapp.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EventsFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
